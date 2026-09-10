aae107fe554472110c9ddca5ab302fbaf66dadfc
Chao Wang <postbox4me@gmail.com>
2026-09-10 19:56:20 +1000

    Update Home brand heading to My Little Days

--- App.tsx
+++ App.tsx
@@ -474,7 +474,7 @@ function BabyApp({
                     fontWeight: "700",
                   }}
                 >
-                  LITTLE DAYS · 小日子
+                  MY LITTLE DAYS · 我的小日子
                 </T>
                 {tab !== "settings" ? (
                   <T
